6375d5520a52c140bd40f89fc2307f8f67a7ccbc
Mike Johnson <mike@rayjohnson.me>
2023-03-10 17:13:27 -0700

    UPdate

--- scripts/04_setup_cluster.sh
+++ scripts/04_setup_cluster.sh
@@ -5,3 +5,5 @@ sudo kubeadm init --pod-network-cidr=10.0.0.0/16
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+export KUBECONFIG=/etc/kubernetes/admin.conf
--- scripts/99_troubleshoot.sh
+++ scripts/99_troubleshoot.sh
@@ -12,3 +12,31 @@ sudo /usr/sbin/execsnoop-bpfcc -n runc
 # runc             7999   7974     0 /usr/local/bin/runc --root /run/containerd/runc/k8s.io --log /run/containerd/io.containerd.runtime.v2.task/k8s.io/b6725c857dd7492a37f73038db80b7443ea1ed9c24b2d98a4b4e15ee8dad24c4/log.json --log-format json --systemd-cgroup start b6725c857dd7492a37f73038db80b7443ea1ed9c24b2d98a4b4e15ee8dad24c4
 # runc             8028   7974     0 /usr/local/bin/runc --root /run/containerd/runc/k8s.io --log /run/containerd/io.containerd.runtime.v2.task/k8s.io/7168c8744f0d2212fac0123f874e348972afae8d2f96a225daa4bae5f593725a/log.json --log-format json --systemd-cgroup create --bundle /run/containerd/io.containerd.runtime.v2.task/k8s.io/7168c8744f0d2212fac0123f874e348972afae8d2f96a225daa4bae5f593725a --pid-file /run/containerd/io.containerd.runtime.v2.task/k8s.io/7168c8744f0d2212fac0123f874e348972afae8d2f96a225daa4bae5f593725a/init.pid --console-socket /tmp/pty2458939095/pty.sock 7168c8744f0d2212fac0123f874e348972afae8d2f96a225daa4bae5f593725a
 # runc             8047   7974     0 /usr/local/bin/runc --root /run/containerd/runc/k8s.io --log /run/containerd/io.containerd.runtime.v2.task/k8s.io/7168c8744f0d2212fac0123f874e348972afae8d2f96a225daa4bae5f593725a/log.json --log-format json --systemd-cgroup start 7168c8744f0d2212fac0123f874e348972afae8d2f96a225daa4bae5f593725a
+
+# https://stackoverflow.com/questions/53394201/kubernetes-context-is-not-set
+CLUSTER_NAME="kubernetes"
+KCONFIG=admin.conf
+KUSER="kubernetes-admin"
+KCERT=admin
+
+cd /etc/kubernetes/
+
+$ kubectl config set-cluster ${CLUSTER_NAME} \
+  --certificate-authority=pki/ca.crt \
+  --embed-certs=true \
+  --server=https://${KUBERNETES_PUBLIC_ADDRESS}:6443 \
+  --kubeconfig=${KCONFIG}
+
+$ kubectl config set-credentials kubernetes-admin \
+  --client-certificate=admin.crt \
+  --client-key=admin.key \
+  --embed-certs=true \
+  --kubeconfig=/etc/kubernetes/admin.conf
+
+$ kubectl config set-context ${KUSER}@${CLUSTER_NAME} \
+  --cluster=${CLUSTER_NAME} \
+  --user=${KUSER} \
+  --kubeconfig=${KCONFIG}
+
+$ kubectl config use-context ${KUSER}@${CLUSTER_NAME} --kubeconfig=${KCONFIG}
+$ kubectl config view --kubeconfig=${KCONFIG}
